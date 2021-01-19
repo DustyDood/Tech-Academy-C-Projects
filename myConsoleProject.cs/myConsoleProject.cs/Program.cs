b4380b8c6957cb45a5a2d0aea6d2f16ea0a8c488
Dusty Zoland <DikaiosuneZ@gmail.com>
2021-01-19 15:07:26 -0800

    Move Initial Assignments
    Condense my initial Tech Academy assignments into my C# repository

---
+++ myConsoleProject.cs/myConsoleProject.cs/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace myConsoleProject.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "! Look as I can include \" instaed of ending the string!");
+            Console.Read();
+        }
+    }
+}
