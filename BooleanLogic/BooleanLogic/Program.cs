9d885b0d035c18506aa22b976cb7e7829bde986a
Dusty Zoland <DikaiosuneZ@gmail.com>
2021-01-21 11:20:27 -0800

    Create Boolean Logic Insurance Approval Program

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+            //Console.ReadLine();
+
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(false || false);
+            //Console.ReadLine();
+
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+            //Console.ReadLine();
+
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+            //Console.ReadLine();
+
+            //Console.WriteLine(true ^ true);
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(false ^ false);
+            //Console.ReadLine();
+
+            Console.WriteLine("Car Insurance Approval");
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? Only enter true or false");
+            bool DUICheck = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            bool eligibilityCheck = (age > 15) && (!DUICheck) && (tickets < 4);
+            Console.WriteLine(eligibilityCheck);
+            Console.ReadLine();
+
+
+        }
+    }
+}
