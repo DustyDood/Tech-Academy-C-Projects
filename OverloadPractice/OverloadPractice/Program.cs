b373359c47ea0227bbb04d6e2e6a1104869611b6
Dusty Zoland <DikaiosuneZ@gmail.com>
2021-01-25 14:23:10 -0800

    Create Overload Practice Assignment

---
+++ OverloadPractice/OverloadPractice/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //First we start with an int
+            VariousOverloads variousOverloads = new VariousOverloads();
+            Console.WriteLine("Want to see some overloads in action? First give me a number!");
+            int firstOverloadReturned = Convert.ToInt32(Console.ReadLine());
+            //Since we don't have static on the class VariousOverloads's methods anymore, we can use this notation!
+            //I was running into an error trying to use instance references for static properties, so removing static made this work.
+            int firstAnswer = variousOverloads.overloadMethod(firstOverloadReturned);
+            Console.WriteLine("Your number plus 26 returns: {0}", firstAnswer);
+            Console.ReadLine();
+
+            //Now for a decimal
+            Console.WriteLine("Now we change a decimal around! Enter a number below.");
+            decimal secondOverloadReturned = Convert.ToDecimal(Console.ReadLine());
+            decimal secondAnswer = variousOverloads.overloadMethod(secondOverloadReturned);
+            Console.WriteLine("Your decimal minus 42.24 returns {0}", secondAnswer);
+            Console.ReadLine();
+
+            //Lastly, a string! Which is kind of what we are already doing since Console.ReadLine() is a string normally...
+            Console.WriteLine("Last but not least, give me one number to multiply!");
+            string thirdOverloadReturned = Console.ReadLine();
+            int thirdAnswer = variousOverloads.overloadMethod(thirdOverloadReturned);
+            Console.WriteLine("Your number multiplied by 2 returns {0}", thirdAnswer);
+            Console.WriteLine("And there we go! Isn't overloading fun?");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ OverloadPractice/OverloadPractice/VariousOverloads.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadPractice
+{
+    class VariousOverloads
+    {
+        public int overloadMethod (int myInt)
+        {
+            int numberToReturn = myInt;
+            numberToReturn += 26;
+            return numberToReturn;
+        }
+
+        public decimal overloadMethod (decimal myDecimal)
+        {
+            decimal numberToReturn = myDecimal;
+            numberToReturn -= 42.24m;
+            return numberToReturn;
+        }
+
+        public int overloadMethod (string myString)
+        {
+            int prestoChangeO;
+            string stringGiven = myString;
+            try
+            {
+                prestoChangeO = Convert.ToInt32(stringGiven);
+            }
+            catch
+            {
+                Console.WriteLine("Whatever string you passed can't become an int, so I'm giving you the int 34!");
+                prestoChangeO = 34;
+            }
+
+            prestoChangeO *= 2;
+            return prestoChangeO;
+        }
+    }
+}
