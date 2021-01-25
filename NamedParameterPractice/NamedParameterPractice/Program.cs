c8fc490868f13a5f42179cb76bc1851e880e2035
Dusty Zoland <DikaiosuneZ@gmail.com>
2021-01-25 14:57:29 -0800

    Create NamedParameterPractice Assignment

---
+++ NamedParameterPractice/NamedParameterPractice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedParameterPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VoidMathClass voidMathClass = new VoidMathClass();
+
+            //I guess we're showing that we can call the parameters in a different order as long as they're named?
+            //Here's the first one, which is the default.
+            voidMathClass.mathMagic(100, 20);
+
+            //Now here's the second call, but I can switch the first and second by using named parameters?
+            voidMathClass.mathMagic(secondNumber: 100, firstNumber: 20);
+
+            //Success! Even though the number 100 comes first in both calls, since we give it the name secondNumber, it performs the secondNumber calls.
+        }
+    }
+}
---
+++ NamedParameterPractice/NamedParameterPractice/VoidMathClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedParameterPractice
+{
+    class VoidMathClass
+    {
+        public void mathMagic(int firstNumber, int secondNumber)
+        {
+            //I feel like I may be misreading the instructions, but it says do a math operation on the first and display the second...
+            //Here's the math operation. Is this one not supposed to be displayed at all..?
+            Console.WriteLine("Watch as we add 104 to your first number!");
+            Console.WriteLine("{0} + 104 = {1}", firstNumber, firstNumber + 104);
+
+            //And here's the 2nd number, which will just match what the user put in...
+            Console.WriteLine("Also, here is your second number: {0}", secondNumber);
+            Console.ReadLine();
+        }
+    }
+}
