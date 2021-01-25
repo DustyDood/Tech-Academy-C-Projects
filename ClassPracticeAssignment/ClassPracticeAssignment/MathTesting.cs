fbaaa2d07da4251cd82a78922d68594706324c82
Dusty Zoland <DikaiosuneZ@gmail.com>
2021-01-25 12:46:29 -0800

    Create class and method testing assignment

---
+++ ClassPracticeAssignment/ClassPracticeAssignment/MathTesting.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPracticeAssignment
+{
+    class MathTesting
+    {
+        public static int AdditionTesting(int addNumber)
+        {
+            int numberToReturn = addNumber;
+            numberToReturn += 20;
+            return numberToReturn;
+        }
+
+        public static int SubtractionTesting(int subtractNumber)
+        {
+            int numberToReturn = subtractNumber;
+            numberToReturn -= 17;
+            return numberToReturn;
+        }
+
+        public static int MultiplicationTesting(int multiplierNumber)
+        {
+            int numberToReturn = multiplierNumber;
+            numberToReturn *= 4;
+            return numberToReturn;
+        }
+    }
+}
---
+++ ClassPracticeAssignment/ClassPracticeAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPracticeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number do you want to do some math on?");
+            int numberTesting = Convert.ToInt32(Console.ReadLine());
+
+            int addResult = MathTesting.AdditionTesting(numberTesting);
+            Console.WriteLine("If we add 20 to your number, we get " + addResult);
+
+            int subtractResult = MathTesting.SubtractionTesting(numberTesting);
+            Console.WriteLine("If we subtract 17 from your number, we get " + subtractResult);
+
+            int multiplicationResult = MathTesting.MultiplicationTesting(numberTesting);
+            Console.WriteLine("Finally, if we multiply your number by 4, we get " + multiplicationResult);
+
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
