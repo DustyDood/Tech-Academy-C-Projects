47a5088a6b10dcf81eb31f09172e86a7bc412ad6
Dusty Zoland <DikaiosuneZ@gmail.com>
2021-01-26 10:30:49 -0800

    Update TwentyOneAssignment with abstract and virtual keywords

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -10,33 +10,29 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-
+            TwentyOneGame twentyOneGame = new TwentyOneGame();
+            twentyOneGame.Players = new List<string>() { "Jesse", "Bill", "Bob" };
+            twentyOneGame.ListPlayers();
             Deck deck = new Deck();
-            deck = Shuffle(deck);
-
-            Console.WriteLine(deck.Cards.Count);
+            deck.Shuffle(3);
 
             foreach (Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
             }
-
+            Console.WriteLine(deck.Cards.Count);
             Console.ReadLine();
         }
 
-        public static Deck Shuffle(Deck deck)
-        {
-            List<Card> TempList = new List<Card>();
-            Random random = new Random();
 
-            while (deck.Cards.Count > 0)
-            {
-                int randomIndex = random.Next(0, deck.Cards.Count);
-                TempList.Add(deck.Cards[randomIndex]);
-                deck.Cards.RemoveAt(randomIndex);
-            }
-            deck.Cards = TempList;
-            return deck;
-        }
+
+        //public static Deck Shuffle(Deck deck, int times)
+        //{
+        //    for (int i = 0; i < times; i++)
+        //    {
+        //        deck = Shuffle(deck);
+        //    }
+        //    return deck;
+        //}
     }
 }
