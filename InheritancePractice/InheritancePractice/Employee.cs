acae720abe89337db7ca94236a29ce596e1df801
Dusty Zoland <DikaiosuneZ@gmail.com>
2021-01-25 18:03:27 -0800

    Create Inheritance Assignment

---
+++ InheritancePractice/InheritancePractice/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritancePractice
+{
+    class Employee : Person
+    {
+        public int ID { get; set; }
+        //How strange... We give Employee the ID property but we don't actually call it this assignment
+    }
+}
---
+++ InheritancePractice/InheritancePractice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritancePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Dusty = new Person
+            {
+                FirstName = "Dusty",
+                LastName = "Zoland"
+            };
+
+            Dusty.SayName();
+
+            Person Sample = new Person
+            {
+                FirstName = "Sample",
+                LastName = "Student"
+            };
+
+            Sample.SayName();
+        }
+    }
+}
