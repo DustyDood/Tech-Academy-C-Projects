d0bdabd73f8c0d1af0c7349c52c78cc35554d4d4
Dusty Zoland <DikaiosuneZ@gmail.com>
2021-01-27 11:48:31 -0800

    Create Basic Struct assignment

---
+++ StructPractice/StructPractice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating an instance of the Number struct.
+            Number testing = new Number();
+            //Assigning an amount to our new instance
+            testing.Amount = 2.43m;
+            //Writing the amount to the console.
+            Console.WriteLine(testing.Amount);
+            Console.ReadLine();
+
+        }
+    }
+
+    public struct Number
+    {
+        public decimal Amount { get; set; }
+
+    }
+}
