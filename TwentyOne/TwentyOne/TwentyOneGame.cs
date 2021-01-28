7400ab406a4e0ffb50df3f3816faadc724223267
Dusty Zoland <DikaiosuneZ@gmail.com>
2021-01-28 15:03:28 -0800

    Add method for restarting game if bust or blackjack.

--- TwentyOne/TwentyOne/TwentyOneGame.cs
+++ TwentyOne/TwentyOne/TwentyOneGame.cs
@@ -8,9 +8,195 @@ namespace TwentyOne
 {
     class TwentyOneGame : Game, IWalkAway
     {
+        //Dealer is created here so that the dealer is specific to the TwentyOneGame
+        public TwentyOneDealer Dealer { get; set; }
+        
         public override void Play()
         {
-            throw new NotImplementedException();
+            Dealer = new TwentyOneDealer();
+            foreach (Player player in Players)
+            {
+                player.Hand = new List<Card>();
+                player.Stay = false;
+            }
+            Dealer.Hand = new List<Card>();
+            Dealer.Stay = false;
+            Dealer.Deck = new Deck();
+            Dealer.Deck.Shuffle();
+            Console.WriteLine("Place your bet!");
+            
+            foreach (Player player in Players)
+            {
+                int bet = Convert.ToInt32(Console.ReadLine());
+                bool successfullyBet = player.Bet(bet);
+                if (!successfullyBet)
+                {
+                    //No value to return, so this just ends the method
+                    return;
+                }
+                Bets[player] = bet;
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Dealing");
+                foreach (Player player in Players)
+                {
+                    Console.Write(" {0}: ", player.Name);
+                    Dealer.Deal(player.Hand);
+                    if (i == 1)
+                    {
+                        bool blackJack = TwentyOneRules.CheckForBlackJack(player.Hand);
+                        if (blackJack)
+                        {
+                            Console.WriteLine("Blackjack! {0} wins {1}", player.Name, Convert.ToInt32(Bets[player]*1.5));
+                            player.Balance += Convert.ToInt32((Bets[player] * 1.5) + Bets[player]);
+                            return;
+
+                        }
+                    }
+                }
+                Console.WriteLine("Dealer: ");
+                Dealer.Deal(Dealer.Hand);
+                if (i == 1)
+                {
+                    bool blackJack = TwentyOneRules.CheckForBlackJack(Dealer.Hand);
+                    if (blackJack)
+                    {
+                        Console.WriteLine("Dealer has Blackjack! Everyone loses!");
+                        foreach (KeyValuePair<Player, int> entry in Bets)
+                        {
+                            Dealer.Balance += entry.Value;
+                        }
+
+
+                        //TRYING THE METHOD ADD HERE
+                        foreach (Player player in Players)
+                        {
+                            PlayAgain(player);
+                        }
+
+
+                        return;
+                    }
+                }
+            }
+            foreach (Player player in Players)
+            {
+                while (!player.Stay)
+                {
+                    Console.WriteLine("Your cards are: ");
+                    foreach (Card card in player.Hand)
+                    {
+                        Console.Write("{0} ", card.ToString());
+                    }
+                    Console.WriteLine("\n\nHit or stay?");
+                    string answer = Console.ReadLine().ToLower();
+                    if (answer == "stay")
+                    {
+                        player.Stay = true;
+                        break;
+                    }
+                    else if (answer == "hit")
+                    {
+                        Dealer.Deal(player.Hand);
+                    }
+                    bool busted = TwentyOneRules.IsBusted(player.Hand);
+                    if (busted)
+                    {
+                        Dealer.Balance += Bets[player];
+                        Console.WriteLine("{0} Busted! You lose your bet of {1}. Your balance is now {2}", player.Name, Bets[player], player.Balance);
+                        Console.WriteLine("Do you want to play again?");
+                        answer = Console.ReadLine().ToLower();
+                        if (answer == "yes" || answer == "yeah")
+                        {
+                            player.isActivelyPlaying = true;
+                            return;
+                        }
+                        else
+                        {
+                            player.isActivelyPlaying = false;
+                            return;
+                        }
+                    }
+                }
+            }
+            Dealer.isBusted = TwentyOneRules.IsBusted(Dealer.Hand);
+            Dealer.Stay = TwentyOneRules.ShouldDealerStay(Dealer.Hand);
+            while (!Dealer.Stay && !Dealer.isBusted)
+            {
+                Console.WriteLine("Dealer is hitting...");
+                Dealer.Deal(Dealer.Hand);
+                Dealer.isBusted = TwentyOneRules.IsBusted(Dealer.Hand);
+                Dealer.Stay = TwentyOneRules.ShouldDealerStay(Dealer.Hand);
+
+            }
+            if (Dealer.Stay)
+            {
+                Console.WriteLine("Dealer is staying.");
+            }
+            if (Dealer.isBusted)
+            {
+                Console.WriteLine("Dealer Busted!");
+                foreach (KeyValuePair<Player, int> entry in Bets)
+                {
+                    Console.WriteLine("{0} won {1}", entry.Key.Name, entry.Value);
+                    Players.Where(x => x.Name == entry.Key.Name).First().Balance += (entry.Value * 2);
+                    Dealer.Balance -= entry.Value;
+
+                    //I'm going to add in the cycle loop thing here since it's not calling the code below!
+                    //Technically, it shouldn't be calling since the dealer would lose twice, but we do need to 
+                    //implement some way the user can choose if they want to play again...
+
+                    foreach (Player player in Players)
+                    {
+                        Console.WriteLine("Play again?");
+                        string answer = Console.ReadLine().ToLower();
+                        if (answer == "yes" || answer == "yeah")
+                        {
+                            player.isActivelyPlaying = true;
+                        }
+                        else
+                        {
+                            player.isActivelyPlaying = false;
+                        }
+                    }
+                }
+                return;
+            }
+            //For some reason, if the dealer busts with the code above, the code below checking to see if they lost doesn't call!
+            //This prevents the user from selecting if they want to play again...
+            foreach (Player player in Players)
+            {
+                bool? playerWon = TwentyOneRules.CompareHands(player.Hand, Dealer.Hand);
+                if (playerWon == null)
+                {
+                    Console.WriteLine("Push! No one wins");
+                    player.Balance += Bets[player];
+                    Bets.Remove(player);
+                }
+                else if (playerWon == true)
+                {
+                    Console.WriteLine("{0} won {1}!", player.Name, Bets[player]);
+                    player.Balance += (Bets[player] * 2);
+                    Dealer.Balance -= Bets[player];
+                }
+                else
+                {
+                    Console.WriteLine("Dealer wins {0}!", Bets[player]);
+                    Dealer.Balance += Bets[player];
+
+                }
+                Console.WriteLine("Play again?");
+                string answer = Console.ReadLine().ToLower();
+                if (answer == "yes" || answer == "yeah")
+                {
+                    player.isActivelyPlaying = true;
+                }
+                else
+                {
+                    player.isActivelyPlaying = false;
+                }
+            }
         }
 
         public override void ListPlayers()
@@ -23,5 +209,24 @@ namespace TwentyOne
         {
             throw new NotImplementedException();
         }
+
+        //Maybe we can instead make a method to call if the game is over... That way, we can put it in the blackjacks, the busts, and the actual comparisons.
+        //Experimental fix... we'll see if it actually works.
+        public void PlayAgain(Player player)
+        {
+            Console.WriteLine("Play again?");
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "yes" || answer == "yeah")
+            {
+                player.isActivelyPlaying = true;
+                return;
+            }
+            else
+            {
+                player.isActivelyPlaying = false;
+                return;
+            }
+        }
     }
 }
+ 
