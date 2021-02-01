1b5d5fa291184680beaa02290d4b380de310e2ef
Dusty Zoland <DikaiosuneZ@gmail.com>
2021-02-01 10:01:19 -0800

    Create IOPractice file

---
+++ IOPractice/IOPractice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IOPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number! Any number!");
+
+            //Typically we would convert strings to ints when using them for functions.
+            //In this case, we're just writing to a text file, so no conversion is needed!
+            string userInput = Console.ReadLine();
+
+            using (StreamWriter streamWriter = new StreamWriter(@"C:\Users\Dusty Zoland\Desktop\Testing.txt", true))
+            {
+                streamWriter.WriteLine(userInput);
+            }
+            string fileOutput = File.ReadAllText(@"C:\Users\Dusty Zoland\Desktop\Testing.txt");
+            Console.WriteLine(fileOutput);
+            Console.ReadLine();
+        }
+    }
+}
