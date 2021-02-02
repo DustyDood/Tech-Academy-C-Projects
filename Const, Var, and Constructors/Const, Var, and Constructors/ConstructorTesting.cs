8fcaeb45a3e211a2f40e3b4a5617c3def6f7f436
Dusty Zoland <DikaiosuneZ@gmail.com>
2021-02-01 17:42:00 -0800

    Create basic assignment

---
+++ Const, Var, and Constructors/Const, Var, and Constructors/ConstructorTesting.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Const__Var__and_Constructors
+{
+    public class ConstructorTesting
+    {
+        public ConstructorTesting(string name, int count)
+        {
+            Name = name;
+            Count = count;
+        }
+
+        public ConstructorTesting(string name) : this(name, 10)
+        {
+
+        }
+
+        public string Name { get; set; }
+        public int Count { get; set; }
+        
+        public void Boast()
+        {
+            Console.WriteLine("My name is {0}. I can count to {1}!", Name, Count);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Const, Var, and Constructors/Const, Var, and Constructors/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Const__Var__and_Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string greetingMessage = "Hello, and welcome!";
+            Console.WriteLine(greetingMessage);
+            Console.ReadLine();
+
+            var testmessage = "How are you doing today?";
+            Console.WriteLine(testmessage);
+            Console.WriteLine("C# recognizes that this test message is a {0}", testmessage.GetType());
+            Console.ReadLine();
+
+            //This call uses a constructor call chain to call the constructor with
+            //entering only a string!
+            ConstructorTesting Bobby = new ConstructorTesting("Bobby");
+            Bobby.Boast();
+        }
+    }
+}
