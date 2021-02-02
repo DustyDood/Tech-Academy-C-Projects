50eb2825403cfc7911973af9c6810a145ed86d8c
Dusty Zoland <DikaiosuneZ@gmail.com>
2021-02-01 18:35:32 -0800

    Create multiple catches for exceptions

---
+++ MoreExceptionHandlingPractice/MoreExceptionHandlingPractice/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreExceptionHandlingPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userAge = 0;
+            bool userSuccess = false;
+
+            try
+            {
+                Console.WriteLine("Hello! May I ask for your age?");
+                userSuccess = int.TryParse(Console.ReadLine(), out userAge);
+                if (!userSuccess) throw new AggregateException();
+                if (userAge == 0) throw new DivideByZeroException();
+                if (userAge < 0) throw new ArithmeticException();
+                if (userAge > 150) throw new ArgumentOutOfRangeException();
+                Console.WriteLine("That's nice! I have no idea what to do with this information since I didn't expect you to answer correctly!");
+            }
+            catch (DivideByZeroException) 
+            {
+                Console.WriteLine("Aren't you a little young to be running this program?");
+            }
+            catch (ArithmeticException)
+            {
+                Console.WriteLine("Very funny, but that means you wouldn't exist right now.");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Sorry, but there's no way that you're this old.");
+            }
+            catch (AggregateException)
+            {
+                Console.WriteLine("Next time, please enter only digits and no decimals!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("I'm sorry, but something went wrong. Please try again.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
